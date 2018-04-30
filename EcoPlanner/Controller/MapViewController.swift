9815c39e74e1fb2f7b1a303e2d26f3ca366a60e0
Ruxi Ciortea <ciortearuxandra10@gmail.com>
2018-04-30 22:15:29 +0300

    Centre map on user position

--- EcoPlanner/Controller/MapViewController.swift
+++ EcoPlanner/Controller/MapViewController.swift
@@ -9,14 +9,88 @@
 import UIKit
 import SKMaps
 
-class MapViewController: UIViewController, SKPositionerServiceDelegate {
+class MapViewController: UIViewController, SKMapViewDelegate {
 
+    @IBOutlet weak var mapView: SKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationController?.navigationBar.barTintColor = kNavyBlueColor
+
+        self.mapView.delegate = self
         
-        SKPositionerService.sharedInstance().delegate = self
-//        SKPositionerService.sharedInstance().positionerMode
+        SKPositionerService.sharedInstance().startLocationUpdate()
+        
+        self.mapView.centerOnCurrentPosition()
+        self.mapView.animate(toZoomLevel: 16)
+    }
+
+    func mapView(_ mapView: SKMapView, didRotateWithAngle angle: Float) {
+        self.mapView.settings.showCompass = true
+    }
+
+    func mapViewDidSelectCompass(_ mapView: SKMapView) {
+        self.mapView.animate(toBearing: 0)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.mapView.settings.showCompass = false
+        }
     }
 }
+
+
+//        let view = UIView()
+//        view.frame = CGRect(x: 100, y: 300, width: 300, height: 300)
+//        view.backgroundColor = .red
+//        self.view.addSubview(view)
+//
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 5 ) {
+//            UIView.animate(withDuration: 3, animations: {
+//                view.backgroundColor = .green
+//            }, completion: { (bool) in
+//                UIView.animate(withDuration: 3, animations: {
+//                    view.frame = CGRect(x: 100, y: 500, width: 200, height: 400)
+//                }, completion: { (bool) in
+//                    view.frame = CGRect(x: 100, y: 300, width: 300, height: 300)
+//                })
+//            })
+//        }
+//
+//        var sum = 0
+//
+//        DispatchQueue.main.sync {
+////            for index in 0...10000000 {
+////                print(sqrt(index))
+////                sum = sum + sqrt(index)
+////            }
+//        }
+//
+//        DispatchQueue.global(qos: .background).async {
+////            for index in 0...10000000 {
+////                print(sqrt(index))
+////                sum = sum + sqrt(index)
+////            }
+//            DispatchQueue.main.async(execute: {
+//                // update ui
+//                print(sum)
+//            })
+//        }
+//
+//        print(sum)
+//
+//        sum = 0
+//
+//        DispatchQueue.main.async {
+////            for index in 0..10000000 {
+////                print(sqrt(index))
+////                sum = sum + sqrt(index)
+////            }
+//        }
+//
+//        print(sum)
+//
+//        UIView.animate(withDuration: 10) {
+//            view.backgroundColor = .blue
+//            view.frame = CGRect(x: 50, y: 50, width: 100, height: 100)
+//        }
